28354584225c678113582c628cafddd8f2023a82
Shruti-D <65996050+Shruti-D@users.noreply.github.com>
2022-06-11 11:54:50 +0530

    Update 1_3.C

--- Chapter1/1_3.C
+++ Chapter1/1_3.C
@@ -7,3 +7,13 @@ main()
     for(fahr = 0; fahr <= 300; fahr = fahr + 20)
         printf("%3d %6.1f\n", fahr, (5.0/9.0) * (fahr - 32));
 }
+
+/*Modify the temperature conversion program to print the table 
+in reverse, that is, from 300 degrees to 0*/
+#include <stdio.h>
+main()
+{
+    int fahr;
+    for(fahr = 300; fahr>=0; fahr = fahr - 20)
+        printf("%3d \t %6.1f\n", fahr, (5.0/9.0)*(fahr - 32.0));
+}
